ace75a9eef0383a7c9f112bdaea7de545aeb66bd
Zuma206 <zuma@zuma.eu.org>
2025-08-10 01:27:58 +0100

    Added Router.Set

--- router/router.go
+++ router/router.go
@@ -1,15 +1,46 @@
 package router
 
-type Router[T any] struct{}
+import "strings"
+
+type Router[T any] struct {
+	children map[string]*Router[T]
+	index    *T
+}
 
 func New[T any]() *Router[T] {
-	return &Router[T]{}
+	return &Router[T]{
+		children: map[string]*Router[T]{},
+	}
 }
 
 func (router *Router[T]) Get(path string) (T, bool) {
 	panic("TODO")
 }
 
+func getSegment(route string) (string, string) {
+	segment := strings.TrimLeftFunc(route, func(r rune) bool { return r != '/' })
+	rest := strings.TrimPrefix(route, segment)
+	return segment, rest
+}
+
+func isIndexRoute(path string) bool {
+	return path == "/" || path == ""
+}
+
+func (router *Router[T]) getChild(segment string) *Router[T] {
+	child, ok := router.children[segment]
+	if !ok {
+		router.children[segment] = New[T]()
+		child = router.children[segment]
+	}
+	return child
+}
+
 func (router *Router[T]) Set(route string, value T) {
-	panic("TODO")
+	if isIndexRoute(route) {
+		router.index = &value
+		return
+	}
+	segment, rest := getSegment(route)
+	router.getChild(segment).Set(rest, value)
 }
