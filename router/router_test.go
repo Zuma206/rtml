afc34627ec6e5e8665b1f7d6c24a95a2458bbfaf
Zuma206 <zuma@zuma.eu.org>
2025-08-10 01:36:22 +0100

    Added router failure tests

--- router/router_test.go
+++ router/router_test.go
@@ -9,16 +9,39 @@ var staticRoutes = []string{
 	"/a-path/another",
 }
 
-func TestStaticRoutes(t *testing.T) {
+func newTestRouter() *Router[int] {
 	// Register all routes
 	router := New[int]()
 	for index, route := range staticRoutes {
 		router.Set(route, index)
 	}
+	return router
+}
+
+func TestStaticRoutes(t *testing.T) {
+	router := newTestRouter()
 	// Test all routes
 	for expectedIndex, route := range staticRoutes {
 		if index, ok := router.Get(route); !ok || index != expectedIndex {
 			t.Errorf("failed to match route %s", route)
+		}
+	}
+}
+
+var notFoundRoutes = []string{
+	"/my-page",
+	"/another-page",
+	"/this/page/doesnt/exist",
+	"/test/not",
+	"/a-path/never/a/path",
+}
+
+func TestRouterFailure(t *testing.T) {
+	router := newTestRouter()
+	for _, route := range notFoundRoutes {
+		_, ok := router.Get(route)
+		if ok {
+			t.Errorf("router unexpectedly matched %s", route)
 		}
 	}
 }
