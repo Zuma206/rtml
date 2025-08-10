f18b370a9eb5f8800496a47a0a34bb321ee9c662
Zuma206 <zuma@zuma.eu.org>
2025-08-10 06:03:51 +0100

    Added router printing for debugging

---
+++ router/debug.go
@@ -0,0 +1,25 @@
+package router
+
+import "fmt"
+
+func Print[T any](router *Router[T]) {
+	printDepth(router, 0)
+}
+
+func printDepth[T any](router *Router[T], depth int) {
+	if router.index != nil {
+		printMargin(depth)
+		println("<leaf>")
+	}
+	for key, child := range router.children {
+		printMargin(depth)
+		fmt.Printf("\"%s\"\n", key)
+		printDepth(child, depth+1)
+	}
+}
+
+func printMargin(depth int) {
+	for range depth {
+		print("\t")
+	}
+}
